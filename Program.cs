d64d8d51deef3384a470ddf9d45ade54806afd96
Blaine Fricks <blaine.fricks@live.com>
2015-02-12 13:54:11 -0800

    Added formula for Spherical Coordinates to find Point C.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Spartan_HUD
 {
@@ -11,45 +11,66 @@ namespace Spartan_HUD
         static void Main(string[] args)
         {
             Console.WriteLine("");
-            // p1 values
-            Console.WriteLine("Point 1 x value:");
-            double a1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Point 1 y value:");
-            double a2 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Point 1 z value:");
-            double a3 = Convert.ToDouble(Console.ReadLine());
+            // pointA values
+            Console.WriteLine("Eye Coordinates");
+            Console.Write("x = ");
+            double a1 = Convert.ToDouble(Console.ReadLine()); // Console Input Build
+            // double a1 = 1; // SublimeText 3 console build
+            // Console.WriteLine(a1); // SublimeText 3 console build
+            Console.Write("y = ");
+            double a2 = Convert.ToDouble(Console.ReadLine()); // Console Input Build
+            // double a2 = 1; // SublimeText 3 console build
+            // Console.WriteLine(a2); // SublimeText 3 console build
+            Console.Write("z = ");
+            double a3 = Convert.ToDouble(Console.ReadLine()); // Console Input Build
+            // double a3 = 1; // SublimeText 3 console build
+            // Console.WriteLine(a3); // SublimeText 3 console build
 
-            // p2 values
-            Console.WriteLine("Point 2 x value:");
-            double b1 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Point 2 y value:");
-            double b2 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Point 2 z value:");
-            double b3 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("");
 
-            // Distance from p2 to p3
-            Console.WriteLine("Distance from Point 2 to Point 3:");
-            double distanceBC = Convert.ToDouble(Console.ReadLine());
+            // pointB values
+            Console.WriteLine("Gun Coordinates");
+            Console.Write("x = ");
+            double b1 = Convert.ToDouble(Console.ReadLine()); // Console Input Build
+            // double b1 = 1; // SublimeText 3 console build
+            // Console.WriteLine(b1); // SublimeText 3 console build
+            Console.Write("y = ");
+            double b2 = Convert.ToDouble(Console.ReadLine()); // Console Input Build
+            // double b2 = 1; // SublimeText 3 console build
+            // Console.WriteLine(b2); // SublimeText 3 console build
+            Console.Write("z = ");
+            double b3 = Convert.ToDouble(Console.ReadLine()); // Console Input Build
+            // double b3 = -1; // SublimeText 3 console build
+            // Console.WriteLine(b3); // SublimeText 3 console build
 
-            // Angle at p2
-            Console.WriteLine("Point 2's Angle:");
-            double angleB = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("");
+
+            // Distance from pointB to pointC
+            Console.Write("Distance from Gun to Target: ");
+            double distanceBC = Convert.ToDouble(Console.ReadLine()); // Console Input Build
+            // double distanceBC = 10.53205080756888; // SublimeText 3 console build
+            // Console.WriteLine(distanceBC); // SublimeText 3 console build
+
+            Console.WriteLine("");
+
+            // Angle at pointB
+            Console.WriteLine("Angle's of Gun ");
+            Console.Write("zy angle = ");
+            double phiB = Convert.ToDouble(Console.ReadLine()); // Console Input Build
+            // double phiB = 85.23; // SublimeText 3 console build
+            // Console.WriteLine(phiB); // SublimeText 3 console build
+            Console.Write("xy = ");
+            double thetaB = Convert.ToDouble(Console.ReadLine()); // Console Input Build
+            // double thetaB = 120.45; // SublimeText 3 console build
+            // Console.WriteLine(thetaB); // SublimeText 3 console build
 
             // Three Points
             double[] pointA = { a1, a2, a3 }; // eye coordinate
             double[] pointB = { b1, b2, b3 }; // aim coordinate
+            double[] pointC = sphericalCoordinate(distanceBC, phiB, thetaB); // target coordinate
 
             double distanceAB = distanceBetween(pointA,pointB);
-
-            double[] p1p3Distance = triangleAngleSides(angleB, distanceAB, distanceBC);
-
-            double  angleA      = p1p3Distance[0];
-            double  distanceAC = p1p3Distance[1];
-            double  angleC      = p1p3Distance[2];
-
-            // double angleCAB = angleBetweenVectors(p1,p2);
-
-            double[] pointC = findPointCoordinates(pointA, pointB, distanceAB, angleA); // target coordinate
+            double distanceAC = distanceBetween(pointA,pointC);
 
             Console.WriteLine("");
             Console.WriteLine("Distance From Eye to Gun    =~ {0}ft", distanceAB);
@@ -70,26 +91,15 @@ namespace Spartan_HUD
             double distanceAB = Math.Sqrt( Math.Pow(xd,2) + Math.Pow(yd,2) + Math.Pow(zd,2) );// d = SquareRoot(xd^2 + yd^2 + zd^2)
             return distanceAB;
         }
-        private static double[] findPointCoordinates(double[] pointA, double[] pointB, double distanceAC, double angleCAB)
+        private static double[] sphericalCoordinate(double row, double phi, double theta)
         {
-            // Find the Coordinates of Point C based on Point A, Point B, distance of AB, and distance of BC
-            double v1 = (Math.Cos(angleCAB) * (pointB[0] - pointA[0]) + Math.Sin(angleCAB) * (pointB[1] - pointA[1])) / distanceBetween(pointA, pointB);
-            double v2 = (Math.Cos(angleCAB) * (pointB[1] - pointA[1]) - Math.Sin(angleCAB) * (pointB[0] - pointA[0])) / distanceBetween(pointA, pointB);
-
             // Point C
-            double c1 = pointA[0] + distanceAC * v1; // x
-            double c2 = pointA[1] + distanceAC * v2; // y
-            double c3 = 0; // z
+            double c1 = row * Math.Sin(phi) * Math.Cos(theta); // x
+            double c2 = row * Math.Sin(phi) * Math.Sin(theta); // y
+            double c3 = row * Math.Cos(phi); // z
 
             double[] pointC = { c1, c2, c3 };
             return pointC;
-        }
-        private static double sideAngleSide(double distanceAB, double distanceBC, double angleABC)
-        {
-            // Calculate distance of Unkown Side of Triangle using Side Angle Side (SAS)
-            // AC^2 = AB^2 + BC^2 - 2(AB*BC)cos(angleABC)
-            double distanceAC = Math.Sqrt( Math.Pow(distanceAB,2) + Math.Pow(distanceBC,2) - 2*distanceAB*distanceBC*Math.Cos(angleABC) );
-            return distanceAC;
         }
         private static double dotProduct(double[] vector1, double[] vector2)
         {
@@ -98,25 +108,13 @@ namespace Spartan_HUD
         }
         private static double magnitude(double[] vector)
         {
-            // a^2 = b^2 + c^2 + d^2
-            double b = vector[0];
-            double c = vector[1];
-            double d = vector[2];
-            double magOfVector = Math.Sqrt( Math.Pow(b, 2) + Math.Pow(c, 2) + Math.Pow(d, 2) );
+            // Magnitude or Length of a Vector
+            // |v| = √(x^2 + y^2 + z^2)
+            double x = vector[0];
+            double y = vector[1];
+            double z = vector[2];
+            double magOfVector = Math.Sqrt( Math.Pow(x, 2) + Math.Pow(y, 2) + Math.Pow(z, 2) );
             return magOfVector;
-        }
-        private static double angleBetweenVectors(double[] vector1, double[] vector2)
-        {
-            double angleV1V2 = Math.Acos(dotProduct(vector1,vector2)/(magnitude(vector1)*magnitude(vector2)));
-            return angleV1V2;
-        }
-        private static double[] triangleAngleSides(double angleB, double distanceAB, double distanceBC)
-        {
-            double distanceAC = Math.Sqrt(Math.Cos(angleB) * 2 * distanceBC * distanceAB - Math.Pow(distanceBC,2) - Math.Pow(distanceAB,2)); // b = arccos((y^2 + z^2 - x^2)/2yz)
-            double angleA = Math.Acos((Math.Pow(distanceAC, 2) + Math.Pow(distanceAB, 2) - Math.Pow(distanceBC, 2)) / (2 * distanceAC * distanceAB)); // a = arccos((x^2 + z^2 - y^2)/2xz)
-            double angleC = Math.Acos((Math.Pow(distanceAC, 2) + Math.Pow(distanceBC, 2) - Math.Pow(distanceAB, 2)) / (2 * distanceAC * distanceBC)); // c = arccos((x^2 + y^2 - z^2)/2xy)
-            double[] foundASA = { angleA, distanceAC, angleC };
-            return foundASA;
         }
     }
 }
